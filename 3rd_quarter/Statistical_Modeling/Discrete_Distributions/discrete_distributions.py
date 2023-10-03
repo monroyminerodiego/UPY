e79f1f4e83280f7cc98d53c51b8587c3a7ca3129
Diego Monroy <139028001+monroyminerodiego@users.noreply.github.com>
2023-10-02 21:50:16 -0600

    2023-10-02

--- 3rd_quarter/Statistical_Modeling/Discrete_Distributions/discrete_distributions.py
+++ 3rd_quarter/Statistical_Modeling/Discrete_Distributions/discrete_distributions.py
@@ -10,45 +10,97 @@ class DiscreteDistributions:
         self.miu = float
 
     def Uniformdist(self):
+        """
+        Function to calculate the Uniform Distribution
+
+        Inputs
+        - n <class 'int'>: Number of trials
+        - x <class 'float'>: Number of successes
+
+        Output
+        - result <class 'float'>: The result of the uniform distribution calculus
+        """
         self.n = float(input("Introduzca el número de intentos: "))
         self.x = float(input("Introduzca el número de éxitos: "))
         self.fdex = (1) / (self.n)
-        self.uniformdist_result = self.fdex * self.x
-        print("La distribución uniforme discreta es: ", self.uniformdist_result)
+        self.result = self.fdex * self.x
+        return self.result
         def mean(self):
             self.uniform_mean = (self.n + 1) / 2
     
         
     
     def Poissondist(self):
+        """
+        Function to calculate the Poisson Distribution
+
+        Inputs
+        - x <class 'float'>: Probability of x.
+        - miu <class 'float'>: The expected value of x. 
+
+        Output
+        - result <class 'float'>: The result of the Poisson distribution calculus
+        """
         self.x = int(input("Introduzca la probabilidad de x: "))
         self.miu = float(input("Introduzca el valor esperado de x: "))
-        self.poissondist_result = ((math.e**-self.miu) * (self.miu**self.x)) / (math.factorial(self.x))
-        print("La distribución de Poisson es: ", self.poissondist_result)
+        self.result = ((math.e**-self.miu) * (self.miu**self.x)) / (math.factorial(self.x))
+        return self.result
         
     
     def Binomialdist(self):
+        """
+        Function to calculate the Binomial Distribution
+
+        Inputs
+        - n <class 'int'>: Number of trials
+        - x <class 'float'>: Number of successes
+        - p <class 'float'>: Probability of success
+        - q <class 'float'>: Probability of failure 
+
+        Output
+        - result <class 'float'>: The result of the Binomial distribution calculus
+        """
         self.n = int(input("Introduzca el número de intentos: "))
         self.x = int(input("Introduzca el número de éxitos: "))
         self.p = float(input("Introduzca la probabilidad de éxito: "))
         self.q = float(input("Introduzca la probabilidad de fallo: "))
         self.combination_nx = (math.factorial(self.n)) / (math.factorial(self.x)) * (math.factorial(self.n - self.x))
-        self.binomialdist_result = float((self.combination_nx) * ((self.p**(self.x) * (self.q**(self.n - self.x)))))
-        print("La distribución binomial es: ", self.binomialdist_result)
+        self.result = float((self.combination_nx) * ((self.p**(self.x) * (self.q**(self.n - self.x)))))
+        return self.result
 
     def Bernoullidist(self):
+        """
+        Function to calculate the Bernoulli Distribution
+
+        Inputs
+        - x <class 'float'>: Number of successes
+        - p <class 'float'>: Probability of success
+
+        Output
+        - result <class 'float'>: The result of Bernoulli distribution calculus
+        """
         self.x = float(input("Introduzca 1 si hubo éxito, introduzca 0 si hubo fracaso: "))
         self.p = float(input("Introduzca la probabilidad de éxito: "))
         if self.x == True:
-            self.bernoullidist_success = self.p**1 * ((1 - self.p)**(1-1))
-            print("La distribución de Bernoulli es: ", self.bernoullidist_success)
+            self.result = self.p**1 * ((1 - self.p)**(1-1))
         else :
-            self.bernoullidist_failure = self.p**0 * ((1 - self.p)**(1-0))
-            print("La distribución de Bernoulli es: ", self.bernoullidist_failure)
+            self.result = self.p**0 * ((1 - self.p)**(1-0))
+
+        return self.result
 
     
     def Geometricdist (self):
+        """
+        Function to calculate the Geometric Distribution
+
+        Inputs
+        - x <class 'float'>: Number of successes
+        - p <class 'float'>: Probability of success
+
+        Output
+        - result <class 'float'>: The result of the Geometric distribution calculus
+        """
         self.x = float(input("Introduzca el número de intentos en el cual se quiere averiguar el experimento: "))
         self.p = float(input("Introduzca la probabilidad de éxito: "))
-        self.geometricdist_result = self.p * (1 - self.p)**(self.x - 1)
-        print("La distribución geométrica es: ", self.geometricdist_result)
+        self.result = self.p * (1 - self.p)**(self.x - 1)
+        return self.result
