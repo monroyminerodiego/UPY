36d9075c35d26671f18f924def67b77cca7c832f
monroyminerodiego <diego.monroy.minero@outlook.com>
2023-10-13 20:36:31 -0600

    2023-10-23

--- 3rd_quarter/Data_Structures/Notebook/Non-Linear_Structures/graphs.py
+++ 3rd_quarter/Data_Structures/Notebook/Non-Linear_Structures/graphs.py
@@ -12,8 +12,14 @@ class Node:
         else:
             string = f"'{self.name}' is connected to:\n"
             for connection in self.connections:
-                string += f"\t{'<' if self.connections[connection] == False else '-'} -----> {connection.name}\n"
+                string += f"\t{'<' if self.connections[connection] == False else '-'}-----> {connection.name}\n"
         return string + '\n\n'
+    
+
+
+class Graph(Node):
+    def __init__(self,name):
+        super().__init__(name)
 
     def add_connection(self,node_to_connect,unidirection = True):
         self.connections[node_to_connect] = unidirection
@@ -23,15 +29,15 @@ class Node:
             self.connections.pop(node_to_disconnect)
             string = f"'{node_to_disconnect}' disconnected succesfully"
 
-node1 = Node('a')
-node2 = Node('b')
-node3 = Node('c')
-node4 = Node('d')
-node5 = Node('e')
+node1 = Graph('a')
+node2 = Graph('b')
+node3 = Graph('c')
+node4 = Graph('d')
+node5 = Graph('e')
 
-node1.add_connection(node2,True)
+node1.add_connection(node2,False)
 node2.add_connection(node3)
-node1.add_connection(node4,True)
+node1.add_connection(node4,False)
 node3.add_connection(node5)
 
 print(node1,node2,node3,node4,node5)
