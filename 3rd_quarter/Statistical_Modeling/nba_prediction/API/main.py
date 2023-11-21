97892143dccec8e664bb75139f62d663676e7f13
monroyminerodiego <diego.monroy.minero@outlook.com>
2023-11-21 08:08:59 -0600

    2023-11-21

--- 3rd_quarter/Statistical_Modeling/nba_prediction/API/main.py
+++ 3rd_quarter/Statistical_Modeling/nba_prediction/API/main.py
@@ -1,9 +1,9 @@
-from flask import Flask, jsonify, request
-app = Flask(__name__)
+import flask as fl
+app = fl.Flask(__name__)
 
 @app.route("/")
 def root():
-    return "Hoal"
+    return fl.render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
