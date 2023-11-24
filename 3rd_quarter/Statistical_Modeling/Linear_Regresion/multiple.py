7468ce02e9d894212f4df11a497e4d50a423a3a4
monroyminerodiego <diego.monroy.minero@outlook.com>
2023-11-24 09:35:15 -0600

    2023-11-24

--- 3rd_quarter/Statistical_Modeling/Linear_Regresion/multiple.py
+++ 3rd_quarter/Statistical_Modeling/Linear_Regresion/multiple.py
@@ -1,51 +1,104 @@
 class multiple_linear_regression:
     def __generate_data_table(self,basic_matrix:list):
         '''
+        Private method to generate the data table of the dataset for 1 dependent variable and 4 independent variables.
+
+        INPUT
+        * basic_matrix -> list: Expects a list of tuples with the independent and dependen variables (y, x1, x2, x3, x4).
+
+        OUTPUT
+        * dataframe -> pandas.DataFrame: Returns a pandas dataframe which will be used as the data table.
         '''
         return pd.DataFrame(data={
-            "y"    : [value[0]           for value in basic_matrix],
-            "x1"   : [value[1]           for value in basic_matrix],
-            "x2"   : [value[2]           for value in basic_matrix],
-            "x1x1" : [value[1]**2        for value in basic_matrix],
-            "x2x2" : [value[2]**2        for value in basic_matrix],
-            "x1x2" : [value[1]*value[2]  for value in basic_matrix],
-            "x1y"  : [value[0]*value[1]  for value in basic_matrix],
-            "x2y"  : [value[0]*value[2]  for value in basic_matrix],
+            'y'    : [ value[0]           for value in basic_matrix],
+            'x1'   : [ value[1]           for value in basic_matrix],
+            'x2'   : [ value[2]           for value in basic_matrix],
+            'x3'   : [ value[3]           for value in basic_matrix],
+            'x4'   : [ value[4]           for value in basic_matrix],
+            'x1x1' : [ value[1]**2        for value in basic_matrix],
+            'x1x2' : [ value[1]*value[2]  for value in basic_matrix],
+            'x1x3' : [ value[1]*value[3]  for value in basic_matrix],
+            'x1x4' : [ value[1]*value[4]  for value in basic_matrix],
+            'x2x2' : [ value[2]**2        for value in basic_matrix],
+            'x2x3' : [ value[2]*value[3]  for value in basic_matrix],
+            'x2x4' : [ value[2]*value[4]  for value in basic_matrix],
+            'x3x3' : [ value[3]*value[3]  for value in basic_matrix],
+            'x3x4' : [ value[3]*value[4]  for value in basic_matrix],
+            'x4x4' : [ value[4]**2        for value in basic_matrix],
+            'x1y'  : [ value[0]*value[1]  for value in basic_matrix],
+            'x2y'  : [ value[0]*value[2]  for value in basic_matrix],
+            'x3y'  : [ value[0]*value[2]  for value in basic_matrix],
+            'x4y'  : [ value[0]*value[2]  for value in basic_matrix],
         })
     
     def __generate_sumatories(self):
         '''
+        Private method to generate the sumatories of all columns in the data_table
+
+        INPUT
+        * Expects nothing
+
+        OUTPUT
+        * Returns a tuple with the values of the summatories of every column
         '''
         y     = sum(self.data_table["y"])
-
         x1    = sum(self.data_table["x1"])
         x2    = sum(self.data_table["x2"])
-
-        x1x1  = sum(self.data_table["x1x1"])
-        x2x2  = sum(self.data_table["x2x2"])
-
-        x1x2  = sum(self.data_table["x1x2"])
-
-        x1y   = sum(self.data_table["x1y"])
-        x2y   = sum(self.data_table["x2y"])
+        x3    = sum(self.data_table["x3"])
+        x4    = sum(self.data_table["x4"])
+        x1x1  = sum(self.data_table['x1x1'])
+        x1x2  = sum(self.data_table['x1x2'])
+        x1x3  = sum(self.data_table['x1x3'])
+        x1x4  = sum(self.data_table['x1x4'])
+        x2x2  = sum(self.data_table['x2x2'])
+        x2x3  = sum(self.data_table['x2x3'])
+        x2x4  = sum(self.data_table['x2x4'])
+        x3x3  = sum(self.data_table['x3x3'])
+        x3x4  = sum(self.data_table['x3x4'])
+        x4x4  = sum(self.data_table['x4x4'])
+        x1y   = sum(self.data_table['x1y'])
+        x2y   = sum(self.data_table['x2y'])
+        x3y   = sum(self.data_table['x3y'])
+        x4y   = sum(self.data_table['x4y'])
         
-        return y, x1, x2, x1x1, x2x2, x1x2, x1y, x2y
+        return y, x1, x2, x3, x4, x1x1, x2x2, x1x2, x1x3, x1x4, x2x3, x2x4, x3x3, x3x4, x4x4, x1x2, x1y, x2y, x3y, x4y
     
     def __generate_matrix_and_coeficients(self):
         '''
+        Private method to generate the matrix, according to the summatories, in orden to know the coefficient that denote the effect of the independent variables on the dependent variable
+
+        INPUT
+        * Expects nothing
+
+        OUTPUT
+        * Returns a tuple with matrix, the intercept and the coefficients of every independent variable
         '''
         matrix = [
-            [len(self.data_table),self.sumatory_x1,self.sumatory_x2],
-            [self.sumatory_x1,self.sumatory_x1x1,self.sumatory_x1x2],
-            [self.sumatory_x2,self.sumatory_x1x2,self.sumatory_x2x2]
+            [len(self.data_table),  self.sumatory_x1  ,  self.sumatory_x2  ,  self.sumatory_x3,    self.sumatory_x4  ],
+            [self.sumatory_x1    ,  self.sumatory_x1x1,  self.sumatory_x1x2,  self.sumatory_x1x3,  self.sumatory_x1x4],
+            [self.sumatory_x2    ,  self.sumatory_x1x2,  self.sumatory_x2x2,  self.sumatory_x2x3,  self.sumatory_x2x4],
+            [self.sumatory_x3    ,  self.sumatory_x1x3,  self.sumatory_x2x3,  self.sumatory_x3x3,  self.sumatory_x3x4],
+            [self.sumatory_x4    ,  self.sumatory_x1x4,  self.sumatory_x2x4,  self.sumatory_x3x4,  self.sumatory_x4x4],
         ]
-        a, b1, b2 = np.linalg.solve(matrix,[self.sumatory_y,self.sumatory_x1y,self.sumatory_x2y])
-        return matrix, a, b1, b2
+        a, b1, b2, b3, b4 = np.linalg.solve(matrix,[self.sumatory_y,self.sumatory_x1y,self.sumatory_x2y,self.sumatory_x3y,self.sumatory_x4y])
+        return matrix, a, b1, b2, b3, b4
 
     def __generate_sum_error_squares(self):
         '''
+        Private method to generate 4 columns to 'data_table':{
+            p   : Pronostic of dependent variable,
+            e   : Error / residue of substracting the pronostic to the dependent variable,
+            ee  : Error / residue of substracting the pronostic to the dependent variable multiplied by itself,
+            SCR : Sumatory of regression squares
+        }
+
+        INPUT
+        * Expects nothing
+
+        OUTPUT
+        * Returns the sum of the squares errors column (ee).
         '''
-        self.data_table["p" ]  = [ self.a + (self.b1 * row.x1) + (self.b2 * row.x2)  for row in self.data_table.itertuples()]
+        self.data_table["p" ]  = [ self.a + (self.b1 * row.x1) + (self.b2 * row.x2) + (self.b3 * row.x3) + (self.b4 * row.x4) for row in self.data_table.itertuples()]
         self.data_table["e" ]  = [ row.y - row.p                                     for row in self.data_table.itertuples()]
         self.data_table["ee"]  = [ row.e**2                                          for row in self.data_table.itertuples()]
         self.data_table["SCR"] = [ (row.p-(self.sumatory_y / self.n))**2             for row in self.data_table.itertuples()]
@@ -53,31 +106,67 @@ class multiple_linear_regression:
 
     def __generate_standar_error(self):
         '''
+        Private method to generate the standar error following the formula:
+        sqrt(sumatory of squares error / Number of rows - ( Number of independent variables + 1)).
+
+        INPUT
+        * Expects nothing.
+        
+        OUTPUT
+        * Returns the value of the formula with the correct values.
         '''
-        return math.sqrt((self.sumatory_error_squares)/(self.n - (2 + 1))) # 2 number of independent variables
+        return math.sqrt((self.sumatory_error_squares)/(self.n - (4 + 1))) # 2 number of independent variables
 
     def __generate_multi_determination_coefficient(self):
         '''
+        Private method to generate the sumatory of the residue of substracting the pronostic to the dependent variable multiplied by itself plus the sumatory of regression squares.
+
+        INPUT
+        * Expects nothing
+        
+        OUTPUT
+        * Returns the value of the needed summatory
         '''
         return sum(self.data_table["ee"]) + sum(self.data_table["SCR"])
 
     def __adjust_multi_determination_coefficient(self):
         '''
+        Private method to adjust the multi_determination_coefficient using the formula:
+        1 - (1 - multi_determination_coefficient) * ((Number of rows - 1) / (Number of rows - Number of independent variables - 1)).
+
+        INPUT
+        * Expects nothing.
+        
+        OUTPUT
+        * Returns the value of the formula with the correct values.
         '''
-        return 1 - (1 - self.RR) * ((self.n - 1)/(self.n - 2 - 1)) # 2 porque es el numero de variables independientes
+        return 1 - (1 - self.RR) * ((self.n - 1)/(self.n - 4 - 1)) # 2 number of independent variables
 
     def __init__(self,basic_matrix:list):
         '''
+        Class created to do a multiple linear regression of 1 dependent variable and 4 independent variables.
+
+        INPUT
+        * Expects a list of tuples|lists with the information of every row of the dataset.
+        The tuple|list needs to follow (y, x1, x2, x3, x4) format, being{
+            y              : Dependent variable
+            x1, x2, x3, x4 : Independent variables
+        }
+        
+        OUTPUT
+        * Returns nothing
         '''
         self.data_table = self.__generate_data_table(basic_matrix)
         self.n = len(basic_matrix)
-        self.sumatory_y, self.sumatory_x1, self.sumatory_x2, self.sumatory_x1x1, self.sumatory_x2x2, self.sumatory_x1x2, self.sumatory_x1y, self.sumatory_x2y = self.__generate_sumatories()
-        self.matrix, self.a, self.b1, self.b2 = self.__generate_matrix_and_coeficients()
+        self.sumatory_y, self.sumatory_x1, self.sumatory_x2, self.sumatory_x3, self.sumatory_x4, self.sumatory_x1x1, self.sumatory_x2x2, self.sumatory_x1x2, self.sumatory_x1x3, self.sumatory_x1x4, self.sumatory_x2x3, self.sumatory_x2x4, self.sumatory_x3x3, self.sumatory_x3x4, self.sumatory_x4x4, self.sumatory_x1x2, self.sumatory_x1y, self.sumatory_x2y, self.sumatory_x3y, self.sumatory_x4y = self.__generate_sumatories()
+        self.matrix, self.a, self.b1, self.b2, self.b3, self.b4 = self.__generate_matrix_and_coeficients()
         self.sumatory_error_squares           = self.__generate_sum_error_squares()
         self.standar_error                    = self.__generate_standar_error()
         self.RR                               = sum(self.data_table['SCR']) / self.__generate_multi_determination_coefficient()
         self.RR_adjusted                      = self.__adjust_multi_determination_coefficient()
         self.R                                = math.sqrt(self.RR)
+        
+        # Esto es el R2 calculado con sklearn.metrics.r2_score
         self.R2                               = r2_score(self.data_table['y'],self.data_table['p'])
 
     def __str__(self):
@@ -90,31 +179,17 @@ class multiple_linear_regression:
 
         string += f"\n{'*'*10} Coeficients {'*'*10}\n\na: {self.a:02f}\tb1: {self.b1:02f}\nb2: {self.b2:02f}\n"
 
-        string += f"\nStandar Error of Multiple Estimation: {self.standar_error:02f}"
+        string += f"\nStandar Error of Multiple Estimation: {self.standar_error:02f}\n"
+
         string += f"\nError Squares: {self.sumatory_error_squares:02f}"
-        string += f"\nSum all Error Squares: {self.RR * 100:02f}"
-        string += f"\nSum all Error Squares Adjusted: {self.RR_adjusted * 100:02f}"
+        string += f"\nManual R2: {self.RR * 100:02f}"
+        string += f"\nAdjusted Manual R2: {self.RR_adjusted * 100:02f}\n"
 
-        string += f"\nMultiple Correlation Coefficient: {self.R * 100:02f}"
+        string += f"\nMultiple Correlation Coefficient (R): {self.R * 100:02f}\n"
 
-        string += f"\nR2: {self.R2 * 100:02f}"
+        string += f"\nR2 with sklearn.metrics: {self.R2 * 100:02f}\n"
 
         return string
-    
-    def generate_plot(self):
-        '''
-        '''
-        x = self.data_table['x']
-        y = self.data_table['y']
-        original_line = [self.m * (xi) + self.b for xi in x]
-        positive_adjust = [(self.m+self.new_m) * xi + (self.b+self.new_b) for xi in x]
-        negative_adjust = [(self.m-self.new_m) * xi + (self.b-self.new_b) for xi in x]
-        plt.scatter(x,y)
-        plt.grid()
-        plt.plot(x,original_line)
-        plt.plot(x,positive_adjust)
-        plt.plot(x,negative_adjust)
-        plt.show()
 
 
 if __name__ == '__main__':
@@ -122,7 +197,6 @@ if __name__ == '__main__':
     from sklearn.metrics import r2_score
     os.system('cls')
     # file = pd.read_csv('Databases/multiple.csv')
-    file = pd.read_csv('../nba_prediction/Databases/cleaned_raw_data.csv')
-    data = [(row.net_orating,row.pts,row.ast) for row in file.itertuples()]
+    file = pd.read_csv('../nba_prediction/Databases/cleaned_raw_data_o.csv')
+    data = [(row.net_rating,row.pts,row.reb,row.ast,row.usg_pct) for row in file.itertuples()]
     multiple = multiple_linear_regression(data)
-    print(multiple)
--- 3rd_quarter/Statistical_Modeling/nba_prediction/Data_Cleaning/cleaning_class.py
+++ 3rd_quarter/Statistical_Modeling/nba_prediction/Data_Cleaning/cleaning_class.py
@@ -2,68 +2,83 @@ class Cleaning:
 
     def __clean_raw_data(self):
         '''
-        Private method to make the cleaning of the raw_data_file
+        Private method to make the cleaning of the raw_data_file. First it only selects the 'USA' players and then it selects the columns that match with the selected correlation.
 
-        Inputs
+        INPUT
         - Excepts nothing
 
-        Output
+        OUTPUT
         - Returns a pandas.DataFrame with the cleaned data
         '''
         # Selecting only 'USA' players        
         df = self.raw_file.query("country == 'USA'")
 
-        # Checking which columns are related to 'net_orating'
-        columns_with_needed_correlation = [row.Index for row in self.raw_file.corr().itertuples() if row.net_orating >= self.correlation_in_columns]
+        # Checking which columns are related to 'net_rating'
+        columns_with_needed_correlation = [row.Index for row in self.raw_file.corr().itertuples() if row.net_rating >= self.correlation_in_columns]
 
         # Adding season to columns
-        columns_with_needed_correlation.append('season')
+        # columns_with_needed_correlation.append('season')
 
         # Selecting only features that we need from our df
         df = df[columns_with_needed_correlation]
 
-        df.drop(axis=1,labels='gp',inplace=True)
+        if 'gp' in df.columns: df.drop(axis=1,labels='gp',inplace=True)
 
         # Creating the dictionary to map the categorical data
-        dict_season = {}
-        counter = 1
-        for i in sorted(set(df['season'])):
-            dict_season[i] = counter
-            counter += 1
+        # dict_season = {}
+        # counter = 1
+        # for i in sorted(set(df['season'])):
+        #     dict_season[i] = counter
+        #     counter += 1
 
-        # Changing to categorical the 'season' feature
-        df['season'] = df['season'].apply(lambda x: dict_season[x])
+        # # Changing to categorical the 'season' feature
+        # df['season'] = df['season'].apply(lambda x: dict_season[x])
 
+        print(f"Correlation of cleaned data\n{df.corr()}\n\n")
         return df
 
     def __estandarizing_data(self):
         '''
         Private method to standarize data
 
-        Input
+        INPUT
         - Expects nothing
 
-        Output
+        OUTPUT
         - Returns a dataframe with the standarized data
         '''
-        df = self.df[:-1]
+        df = self.df
         scaler = StandardScaler()
         df = scaler.fit_transform(df)
         df = pd.DataFrame(df,columns=list(self.df.columns))
         # df['season'] = self.df['season']
         return df
 
-    def __init__(self, raw_file_path:str, correlation_in_columns:float, download_mode:bool = False, cleaned_file_path:str = ''):
+    def __normalizing_data(self):
+        '''
+        Private method that normalice data.
+
+        INPUT
+        * Expects nothing.
+
+        OUTPUT
+        * Returns a pandas.DataFrame with the data normalized.
+        '''
+        return pd.DataFrame(data=normalize(self.df, axis=0),columns=[column for column in self.df.columns])
+
+    def __init__(self, raw_file_path:str, correlation_in_columns:float, download_mode:bool = False, cleaned_file_path:str = '', estandarize_data:bool = True, normalize_data:bool = False):
         '''
         Class created to do the cleaning of a CSV file
 
-        Input
+        INPUT
         - raw_file_path          [str]   : Expects the path where the raw_data is located.
         - correlation_in_columns [float] : Expects a value between 0 and 1 that will be used to select columns that fits in that range.
-        - download_mode          [bool]  : If set to True, then at the end of the cleaning, a CSV file will be stored in 'cleaned_file_path'
+        - download_mode          [bool]  : If set to 'True', then at the end of the cleaning, a CSV file will be stored in 'cleaned_file_path'. Default set to 'False'
         - cleaned_file_path      [str]   : Expects the path where the cleaned_data will be located.
+        - estandarize_data       [bool]  : If set to 'True', the raw data will be estandarized. Default set to 'True'
+        - normalize_data         [bool]  : If set to 'True', the raw data will be normalized. Default set to 'False'
 
-        Output
+        OUTPUT
         - Returns nothing, but when 'download_mode' is set to True, then it downloads a CSV File
         '''
         # Defining parameters
@@ -73,25 +88,54 @@ class Cleaning:
         self.raw_file = pd.read_csv(raw_file_path,index_col=0)
         self.df = self.__clean_raw_data()
 
-        # Estandarizing data
-        self.normalized_data = self.__estandarizing_data()
+        # Uncomment to see the distribution of the variables
+        # self.get_distribution()
 
-        # Saving document
-        if download_mode:
-            self.normalized_data.to_csv(f'{cleaned_file_path}cleaned_raw_data.csv')
+        # Estandarizing data
+        if estandarize_data: self.df = self.__estandarizing_data()
+
+        # Normalizing data
+        if normalize_data: self.df = self.__normalizing_data()
+
+        # # Saving document
+        if download_mode: self.df.to_csv(f'{cleaned_file_path}cleaned_raw_data{version}.csv')
+
+    def get_distribution(self):
+        '''
+        Public method to get a graph with the histogram of every column
+
+        INPUT
+        * Expects nothing
+
+        OUTPUT
+        * Returns nothing, but shows the plot with the histograms
+        '''
+        x = 1
+        for column in self.df.columns:
+            plt.subplot(round(len(self.df.columns)/3)+1 if len(self.df.columns)%3 != 0 else len(self.df.columns)/3,3,x)
+            plt.hist(self.df[column])
+            plt.title(column)
+            x += 1
+        plt.show()
 
 
 if __name__ == '__main__':
     import os; os.system('cls')
     import pandas as pd
+    import matplotlib.pyplot as plt
     from sklearn.preprocessing import StandardScaler
+    from sklearn.preprocessing import normalize
 
-    input = '../Databases/raw_data.csv'
-    correlation = 0.49
+    version = '_o'
+    input = f'../Databases/raw_data{version}.csv'
+    correlation = 0.56
     output_path = '../Databases/'
     
-    Cleaning(raw_file_path = input,
+    
+    Cleaning(raw_file_path          = input,
              correlation_in_columns = correlation,
-             cleaned_file_path=output_path,
-             download_mode=True
-             )
+             cleaned_file_path      = output_path,
+             download_mode          = False,
+             estandarize_data       = True,
+             normalize_data         = False
+             ).get_distribution()
