2c3155c971f03ab85ba30507622b5dd0fdd017bf
monroyminerodiego <diego.monroy.minero@outlook.com>
2023-12-04 16:32:02 -0600

    2023-12-04

--- 3rd_quarter/Statistical_Modeling/nba_prediction/API/main.py
+++ 3rd_quarter/Statistical_Modeling/nba_prediction/API/main.py
@@ -10,31 +10,37 @@ ALLOWED_EXTENSIONS = set(['csv'])
 
 @app.route("/")
 def root():
+    global uploaded_file_path
     os.system('cls')
     print(f'\n\n\n{"*"*15} Esta en vista root {"*"*15}\n\n\n')
     return fl.render_template('views/index.html')
 
-@app.route("/selected",methods=['POST'])
+uploaded_file_path = uploaded_file_path
+
+@app.route("/selected",methods=['POST']) #type: ignore
 def upload():
+    global uploaded_file_path
     print(f'\n\n\n{"*"*15} Esta en vista selected {"*"*15}\n\n\n')
     if fl.request.method == 'POST':
-        global uploaded_file_path
         file = fl.request.files['Name_UploadFile']
-        filename = secure_filename(file.filename)
+        filename = secure_filename(file.filename) #type: ignore
         uploaded_file_path = f"{app.config['UPLOAD_FOLDER']}/{filename}"
         file.save(uploaded_file_path)
         column_names = (((open(uploaded_file_path,'r').readline()).replace('\n','')).split(','))
         return fl.render_template("views/cleaning.html",column_names=column_names,file_path=file)
 
-@app.route("/cleaned",methods=['POST'])
+uploaded_file_path = uploaded_file_path
+
+@app.route("/cleaned?corr")
 def clean():
-    correlation = fl.request.args.get("pct_correlation")
-    file_path = fl.request.args.get("file_path")
-    print(f'\n\n\n{"*"*15} Esta en vista cleaned {"*"*15}\n{type(file_path)}.- {file_path}\n\n\n')
+    global uploaded_file_path
+    file = fl.request.files['corr']
+    print(file)
+    print(f'\n\n\n{"*"*15} Esta en vista cleaned {"*"*15}\n{type(uploaded_file_path)}.- {uploaded_file_path}\n\n\n')
     from static.PY.cleaning_class import Cleaning
     data_cleaned = Cleaning(
-        raw_file_path=file_path,#type:ignore
-        correlation_in_columns=correlation#type:ignore
+        raw_file_path=uploaded_file_path,#type:ignore
+        correlation_in_columns=0.49
     ) 
     image_path = f"{(os.path.splitext(uploaded_file_path)[0]).replace('Downloads','Images')}_distribution.png"
     data_cleaned.save_distribution_image(download_image_path=image_path)
