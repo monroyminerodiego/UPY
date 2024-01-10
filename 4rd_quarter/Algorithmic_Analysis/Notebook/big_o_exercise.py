87fcf7f71dfdf70b81a4008643e877f65fe2be3c
monroyminerodiego <diego.monroy.minero@outlook.com>
2024-01-10 00:08:23 -0600

    2024-01-10

--- 4rd_quarter/Algorithmic_Analysis/Notebook/big_o_exercise.py
+++ 4rd_quarter/Algorithmic_Analysis/Notebook/big_o_exercise.py
@@ -1,19 +1,70 @@
+import os
 from random import randint
 
 class sort_list_big_o:
-    def __init__(self,array:list = [9,1,3,7,5,6,4,8,2,0],target:int = randint(0,9)):
+    def __init__(self,array:list = [9,1,3,7,5,6,4,8,2,0]):
         '''
         '''
         self.array = array
-        self.target = target
+        print(f'Initialized class with array: \n{self.array}')
 
-    def constant_time(self):
+    # ================== METHOD'S ==================
+    def sort_by_constant_time(self,array:list,first:int = 0,second:int = 1):
         '''
+        Sorts an array using a recursive approach.
+
+        ### Args:
+        * `array`: The list to be sorted.
+        * `first`: The index of the first element to compare (default: 0).
+        * `second`: The index of the second element to compare (default: 1).
+
+        ### Returns:
+        - The sorted `array`.
+
+        ### Time complexity:
+        - `O(N^2)` worst-case, `O(N log N)` average-case.
         '''
-        first, second = 0, 1
         while True:
-            if self.array[first] > self.array[second]:
-                pass
+            if array[first] > array[second]:
+                # Saves the original values
+                left = array[second]
+                right = array[first]
+                # Rewrites with the correct order
+                array[second] = right
+                array[first] = left
+
+                array = self.sort_by_constant_time(
+                    array  = array
+                    )
+            else:
+                if second+1 == len(array):
+                    break
+                array = self.sort_by_constant_time(
+                    array  = array,
+                    first  = first  + 1,
+                    second = second + 1
+                    )
+            break
+        return array
+    
+    def get_max_number(self,array:list):
+        '''
+        Finds the maximum number in an array.
+
+        ### Args:
+        * `array`: The list of numbers to search.
+
+        ### Returns:
+        * The maximum number in the `array`.
+
+        ### Time complexity:
+        * `O(N)`
+        '''
+        max_number = 0
+        for index in range(len((array))):
+            if max_number < array[index]: max_number = array[index]
+        return max_number
+
 
         
 
@@ -26,4 +77,14 @@ if __name__ == '__main__':
         * O(n log n): Linearithmic time
         * O(n^2): Quadratic time
     '''
-    test = sort_list_big_o()
+    os.system('cls')
+    test = sort_list_big_o(
+        # array = [4, 2, 7, 1, 9,6]
+        # array = [1, 2, 4, 3, 5, 7, 6,8]
+    )
+
+    sorted_list = test.sort_by_constant_time(test.array)
+
+    get_max_number = test.get_max_number(test.array)
+
+    print(f"\nThe sorted list is {sorted_list} and the max number is {get_max_number}")
--- 4rd_quarter/Database_Management/Final_Project/init.py
+++ 4rd_quarter/Database_Management/Final_Project/init.py
@@ -1,9 +1,17 @@
+'''
+Yahoo Finanzas: https://es-us.finanzas.yahoo.com/screener/new/
+'''
+
+
+import os; os.system('cls')
 import requests
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-test = requests.get(
-    url = 'https://listado.mercadolibre.com.mx/computacion/laptops-accesorios/laptops/laptop_Desde_2050_NoIndex_True'
-)
+driver_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe'
 
+option = webdriver.ChromeOptions()
+# options.add_argument('--start-maximized' )
 
-
-print(test)
+driver = webdriver.Chrome(driver_path,options=option)
