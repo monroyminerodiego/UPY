01410db837023794b02f32f59a93aef5c0f75b22
monroyminerodiego <diego.monroy.minero@outlook.com>
2024-06-13 17:04:05 -0600

    Se cambio para que si encuentra corriendo el docker, no lo elimine y lo vuelva a crear.

--- 5th_quarter/Big_Data/hadoop_container/run.bat
+++ 5th_quarter/Big_Data/hadoop_container/run.bat
@@ -6,21 +6,18 @@ echo Building Docker image...
 docker build -t %IMAGE_NAME% .
 
 echo Checking if container is already running...
-docker ps -a --filter "name=%CONTAINER_NAME%" --format "{{.Names}}" | findstr /r "^%CONTAINER_NAME%$" > nul
+docker ps --filter "name=%CONTAINER_NAME%" --format "{{.Names}}" | findstr /r "^%CONTAINER_NAME%$" > nul
 if %ERRORLEVEL% == 0 (
-    echo Stopping and removing existing container...
-    docker stop %CONTAINER_NAME%
-    docker rm %CONTAINER_NAME%
+    echo Container %CONTAINER_NAME% is already running.
+) else (
+    echo Container is not running, starting a new one...
+    docker run -d --name %CONTAINER_NAME% ^
+    -p 9870:9870 ^
+    -p 9864:9864 ^
+    -p 8042:8042 ^
+    -p 8088:8088 ^
+    -p 10020:10020 ^
+    -p 19888:19888 ^
+    %IMAGE_NAME%
+    echo Container %CONTAINER_NAME% has been started.
 )
-
-echo Running new container...
-docker run -it --rm --name %CONTAINER_NAME% ^
--p 9870:9870 ^
--p 9864:9864 ^
--p 8042:8042 ^
--p 8088:8088 ^
--p 10020:10020 ^
--p 19888:19888 ^
-%IMAGE_NAME%
-
-echo Container has been started.
