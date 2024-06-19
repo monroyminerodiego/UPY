0bfef1ebaf0c09743bc1b7c4ffe576663c2a0713
monroyminerodiego <diego.monroy.minero@outlook.com>
2024-06-18 20:17:54 -0600

    Guardado de cambios para hadoop en docker

--- 5th_quarter/Big_Data/hadoop_container/run.bat
+++ 5th_quarter/Big_Data/hadoop_container/run.bat
@@ -5,19 +5,14 @@ set CONTAINER_NAME=hadoop-container
 echo Building Docker image...
 docker build -t %IMAGE_NAME% .
 
-echo Checking if container is already running...
-docker ps --filter "name=%CONTAINER_NAME%" --format "{{.Names}}" | findstr /r "^%CONTAINER_NAME%$" > nul
-if %ERRORLEVEL% == 0 (
-    echo Container %CONTAINER_NAME% is already running.
-) else (
-    echo Container is not running, starting a new one...
-    docker run -d --name %CONTAINER_NAME% ^
-    -p 9870:9870 ^
-    -p 9864:9864 ^
-    -p 8042:8042 ^
-    -p 8088:8088 ^
-    -p 10020:10020 ^
-    -p 19888:19888 ^
-    %IMAGE_NAME%
-    echo Container %CONTAINER_NAME% has been started.
-)
+echo Container is not running, starting a new one...
+docker run -it --name %CONTAINER_NAME% ^
+-p 9870:9870 ^
+-p 9864:9864 ^
+-p 8042:8042 ^
+-p 8088:8088 ^
+-p 10020:10020 ^
+-p 19888:19888 ^
+%IMAGE_NAME%
+echo Container %CONTAINER_NAME% has been started.
+
