8297fb3996eb29aef624693a35f55b8727829f47
monroyminerodiego <diego.monroy.minero@outlook.com>
2024-06-19 18:28:00 -0600

    Version preliminar. Funciona sin exponer puertos -> ya no pide lo de JAVA

--- 5th_quarter/Big_Data/hadoop_container/run.bat
+++ 5th_quarter/Big_Data/hadoop_container/run.bat
@@ -1,18 +1,25 @@
 @echo off
+cls
 set IMAGE_NAME=hadoop-docker
 set CONTAINER_NAME=hadoop-container
 
 echo Building Docker image...
 docker build -t %IMAGE_NAME% .
 
-echo Container is not running, starting a new one...
+echo Deleting existing container...
+docker rm -f %CONTAINER_NAME%
+
+
+echo Starting a new container...
+@REM docker run -d -P --name %CONTAINER_NAME% %IMAGE_NAME%
 docker run -it --name %CONTAINER_NAME% ^
--p 9870:9870 ^
--p 9864:9864 ^
--p 8042:8042 ^
 -p 8088:8088 ^
--p 10020:10020 ^
--p 19888:19888 ^
+-p 8042:8042 ^
+-p 50070:50070 ^
+-p 50075:50075 ^
+-p 50010:50010 ^
+-p 50020:50020 ^
+-p 50090:50090 ^
 %IMAGE_NAME%
-echo Container %CONTAINER_NAME% has been started.
+
 
