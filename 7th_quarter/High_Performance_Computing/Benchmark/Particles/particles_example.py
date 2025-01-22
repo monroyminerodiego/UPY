43dbe93815b111c6e64394be07988702eb429b34
monroyminerodiego <diego.monroy.minero@outlook.com>
2025-01-21 20:22:01 -0600

    Notas de clase

---
+++ 7th_quarter/High_Performance_Computing/Benchmark/Particles/particles_example.py
@@ -0,0 +1,90 @@
+import os, matplotlib.pyplot as plt
+from matplotlib import animation
+from random import uniform
+import cProfile
+
+class Particle:
+    def __init__(self,x,y,vel):
+        self.x = x
+        self.y = y
+        self.vel = vel
+
+class ParticleSimulator:
+    def __init__(self,particles:list):
+        self.particles = particles
+
+    @profile #type: ignore
+    def evolve(self,dt):
+        timestep = 0.00001 #Time Delta
+        nsteps = int(dt / timestep)
+        for i in range(nsteps):
+            for p in self.particles:
+                # Calculate r
+                r = (p.x**2 + p.y**2)**0.5
+
+                # Calculate speed components
+                v_x = - p.vel * p.y / r
+                v_y = + p.vel * p.x / r
+
+                # Calculate displacement
+                d_x = v_x * timestep
+                d_y = v_y * timestep
+
+                # Update new motions
+                p.x += d_x
+                p.y += d_y
+
+def visualize(simulator):
+    ''' '''
+    X = [p.x for p in simulator.particles]
+    Y = [p.x for p in simulator.particles]
+
+    fig = plt.figure()
+    ax = plt.subplot(111,aspect = 'equal')
+    line, = ax.plot(X,Y,'ro')
+    
+    # Axis limits
+    plt.xlim(-1,1)
+    plt.ylim(-1,1)
+
+    def init():
+        line.set_data([],[])
+        return line,
+    
+    def animate(i):
+        simulator.evolve(0.01)
+        X = [p.x for p in simulator.particles]
+        Y = [p.y for p in simulator.particles]
+
+        line.set_data(X,Y)
+        return line,
+
+    anim = animation.FuncAnimation(
+        fig,
+        animate,
+        init_func=init,
+        interval = 10
+    )
+
+    plt.show()
+
+def benchmark():
+
+    particles = [Particle(uniform(-1.0,1.0),uniform(-1.0,1.0),uniform(-1.0,1.0)) for _ in range(100)]
+
+    simulator = ParticleSimulator(particles)
+    simulator.evolve(0.1)
+
+
+if __name__ == '__main__':
+    os.system('clear')
+    
+    # pr = cProfile.Profile()
+    # pr.enable()
+    benchmark()
+    # pr.disable()
+    # pr.print_stats(sort = 'ncalls')
+
+    # pr.print_stats(sort = 'tottime')
+
+    # pr.print_stats(sort = 'cumtime')
